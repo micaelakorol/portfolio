4826885a2db11e035d110ba39a122387eb07b364
Micaela <micaelakorol@gmail.com>
2025-03-25 15:24:26 -0300

    new button styles

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,29 +1,16 @@
-const css =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715104565/ult-portfolio/css_hbrtvp.webp';
-const firebase =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715104559/ult-portfolio/firebase_ea7jao.webp';
-const js =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715104560/ult-portfolio/js_pjguqg.webp';
-const sqlite =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1736167000/new-portfolio/SQLite_Vector_logo_anbwff.png';
-const react =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715104561/ult-portfolio/react_bs3p9i.webp';
-const form =
-  'https://docs.google.com/forms/d/1r9efbF9XZfEEK7p60ZfmDQZK_NBc7AOXRzn6e6KSI_Q/';
-const download =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715284431/ult-portfolio/download_tnpoo6.svg';
-const styled =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715104558/ult-portfolio/styled_hie7jo.webp';
-const main =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715177175/ult-portfolio/114_t2bk5d.webp';
-const html =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715104566/ult-portfolio/html_pqs9mg.webp';
-const linkedin =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715108366/ult-portfolio/linkedin-svgrepo-com_chljsp.svg';
-const github =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715108412/ult-portfolio/github_j0vyxe.svg';
-const email =
-  'https://res.cloudinary.com/dgqyqd2pa/image/upload/v1715108393/ult-portfolio/email-svgrepo-com_m3mjpy.svg';
+const css = 'https://is.gd/RKS6cU';
+const firebase = 'https://is.gd/ZftDrj';
+const js = 'https://is.gd/XwUvTI';
+const sqlite = 'https://is.gd/YjvF7g';
+const react = 'https://is.gd/wtDuh9';
+const form = 'https://is.gd/mefH9u';
+const download = 'https://is.gd/Ptay2e';
+const styled = 'https://is.gd/865P8L';
+const main = 'https://is.gd/xFBjnm';
+const html = 'https://is.gd/sJ4dG9';
+const linkedin = 'https://is.gd/G1mXEm';
+const github = 'https://is.gd/mzrqdF';
+const email = 'https://is.gd/3Jnoyp';
 export {
   github,
   linkedin,
